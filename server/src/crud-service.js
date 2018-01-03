3496e2fc5bdd5f662b2737805755cdfe4f8b1e9a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-01-03 14:43:00 -0600

    added crud-service and tests for it

---
+++ server/src/crud-service.js
@@ -0,0 +1,109 @@
+// @flow
+
+const MySqlConnection = require('mysql-easier');
+
+const {errorHandler} = require('./util/error-util');
+
+const NOT_FOUND = 404;
+const OK = 200;
+
+function crud(
+  app: express$Application,
+  mySql: MySqlConnection,
+  tableName: string
+) {
+
+  async function deleteAllHandler(
+    req: express$Request,
+    res: express$Response
+  ): Promise<void> {
+    try {
+      await mySql.deleteAll(tableName);
+      res.send();
+    } catch (e) {
+      // istanbul ignore next
+      errorHandler(res, e);
+    }
+  }
+
+  async function deleteByIdHandler(
+    req: express$Request,
+    res: express$Response
+  ): Promise<void> {
+    const {id} = req.params;
+    try {
+      const {affectedRows} = await mySql.deleteById(tableName, id);
+      res.send(String(affectedRows));
+    } catch (e) {
+      // istanbul ignore next
+      errorHandler(res, e);
+    }
+  }
+
+  async function getAllHandler(
+    req: express$Request,
+    res: express$Response
+  ): Promise<void> {
+    try {
+      const rows = await mySql.getAll(tableName);
+      res.send(JSON.stringify(rows));
+    } catch (e) {
+      // istanbul ignore next
+      errorHandler(res, e);
+    }
+  }
+
+  async function getByIdHandler(
+    req: express$Request,
+    res: express$Response
+  ): Promise<void> {
+    const {id} = req.params;
+    try {
+      const type = await mySql.getById(tableName, id);
+      res.status(type ? OK : NOT_FOUND).send(JSON.stringify(type));
+    } catch (e) {
+      // istanbul ignore next
+      errorHandler(res, e);
+    }
+  }
+
+  async function patchHandler(
+    req: express$Request,
+    res: express$Response
+  ): Promise<void> {
+    const {id} = req.params;
+    const changes = req.body;
+    try {
+      const type = await mySql.getById(tableName, id);
+      const newType = {...type, ...changes};
+      await mySql.updateById(tableName, id, newType);
+      res.status(OK).send(JSON.stringify(newType));
+    } catch (e) {
+      // istanbul ignore next
+      errorHandler(res, e);
+    }
+  }
+
+  async function postHandler(
+    req: express$Request,
+    res: express$Response
+  ): Promise<void> {
+    try {
+      const rows = await mySql.insert(tableName, req.body);
+      res.send(JSON.stringify(rows));
+    } catch (e) {
+      // istanbul ignore next
+      errorHandler(res, e);
+    }
+  }
+
+  const URL_PREFIX = '/' + tableName;
+  app.delete(URL_PREFIX, deleteAllHandler);
+  app.delete(URL_PREFIX + '/:id', deleteByIdHandler);
+  app.get(URL_PREFIX, getAllHandler);
+  app.get(URL_PREFIX + '/:id', getByIdHandler);
+  app.patch(URL_PREFIX + '/:id', patchHandler);
+  app.post(URL_PREFIX, postHandler);
+}
+
+module.exports = crud;
---
+++ server/src/crud-service.test.js
@@ -0,0 +1,64 @@
+// @flow
+
+const got = require('got');
+
+const TABLE = 'alert_type';
+const HOST = 'localhost';
+const PORT = 3001;
+const URL_PREFIX = `http://${HOST}:${PORT}/${TABLE}`;
+
+describe('crud-service', () => {
+  const name = 'some alert type';
+  let id;
+
+  beforeEach(async () => {
+    // Exercises deleteAllHandler
+    await got.delete(URL_PREFIX);
+
+    // Test postHandler.
+    const options = {body: {name}, json: true};
+    const res = await got.post(URL_PREFIX, options);
+    id = res.body;
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('getByIdHandler', async () => {
+    const res = await got.get(URL_PREFIX + '/' + id);
+    expect(res.statusCode).toBe(200);
+    const object = JSON.parse(res.body);
+    expect(object.name).toBe(name);
+  });
+
+  test('getAllHandler', async () => {
+    const res = await got.get(URL_PREFIX);
+    expect(res.statusCode).toBe(200);
+    const objects = JSON.parse(res.body);
+    expect(objects.length).toBe(1);
+    const [object] = objects;
+    expect(object.name).toBe(name);
+  });
+
+  test('deleteByIdHandler', async done => {
+    let res = await got.delete(URL_PREFIX + '/' + id);
+    expect(res.statusCode).toBe(200);
+    try {
+      res = await got.get(URL_PREFIX + '/' + id);
+      done.fail('get by id succeeded after delete by id');
+    } catch (e) {
+      expect(e.statusCode).toBe(404);
+      done();
+    }
+  });
+
+  test('patchHandler', async () => {
+    const newName = 'some new name';
+    const options = {body: {name: newName}, json: true};
+    let res = await got.patch(URL_PREFIX + '/' + id, options);
+    expect(res.statusCode).toBe(200);
+
+    res = await got.get(URL_PREFIX + '/' + id);
+    expect(res.statusCode).toBe(200);
+    const object = JSON.parse(res.body);
+    expect(object.name).toBe(newName);
+  });
+});
