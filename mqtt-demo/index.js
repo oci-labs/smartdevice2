a5931abdd2fe82042bb06e1901899900b78e0c67
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-01-11 15:47:18 -0600

    more progress on MQTT client

--- mqtt-demo/index.js
+++ mqtt-demo/index.js
@@ -1,21 +1,78 @@
 // @flow
 
-const mqtt = require('mqtt');
-//const host = 'test.mosquitto.org';
-const host = 'localhost';
-const port = 8883;
-//const client = mqtt.connect('mqtt://test.mosquitto.org');
-const client = mqtt.connect('mqtt://' + host + ':' + port);
+const TRAIN_NAME = 'thejoveexpress';
 
-const topic = 'foo/bar';
+//const mqttHost = TRAIN_NAME + '.local';
+const mqttHost = 'localhost';
+
+const mqtt = require('mqtt');
+const PORT = 1883;
+const client = mqtt.connect('mqtt://' + mqttHost + ':' + PORT);
+
+const topicPrefix = TRAIN_NAME + '/';
+
+function getTopic(name) {
+  return topicPrefix + (name ? name + '/' : '') + 'feedback';
+}
+
+const feedbackTopic = getTopic('');
+const lifecycleTopic = getTopic('lifecycle');
+const lightsOverrideTopic = getTopic('lights/override');
+const lightsPowerTopic = getTopic('lights/power');
+/*
+const enginePowerTopic = getTopic('engine/power');
+const engineCalibrationTopic = getTopic('engine/calibration');
+const lightsAmbientTopic = getTopic('lights/ambient');
+const lightsCalibrationTopic = getTopic('lights/calibration');
+*/
 
 client.on('connect', () => {
+  const topic = TRAIN_NAME + '/#';
   client.subscribe(topic);
-  client.publish(topic, 'Hello from MQTT!');
+  //client.publish(topic, 'Hello from MQTT!');
+  client.publish('thejoveexpress/feedback');
 });
 
+// message is a Buffer
 client.on('message', (topic, message) => {
-  // message is a Buffer
-  console.log(message.toString());
+  console.log('got message for topic', topic);
+  console.log('message length =', message.length);
+
+  switch (topic) {
+    case feedbackTopic:
+      break;
+
+    case lifecycleTopic:
+    case lightsPowerTopic: {
+      const value = message.readInt8(0);
+      console.log('lifecycle =', value);
+      break;
+    }
+
+    case lightsOverrideTopic: {
+      const value = message.readInt32BE(0);
+      console.log('lights override =', value);
+      break;
+    }
+
+    default: {
+      const num = message.readIntBE(0, 8);
+      console.log('num =', num);
+      const den = message.readIntBE(8, 8);
+      console.log('den =', den);
+      break;
+    }
+  }
+
   //client.end();
 });
+
+/*
+thejoveexpress/lifecycle/feedback - boolean, 1 byte
+thejoveexpress/engine/power/feedback - rational
+thejoveexpress/engine/calibration/feedback - rational
+thejoveexpress/lights/ambient/feedback - rational
+thejoveexpress/lights/override/feedback - enum, 4 bytes 0=off, 1=on, 2=auto
+thejoveexpress/lights/power/feedback - boolean, 1 byte
+thejoveexpress/lights/calibration/feedback - rational
+*/
