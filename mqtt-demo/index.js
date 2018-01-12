c47fa6b0ec5f1ef656f833c704a9b645c9d326ce
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-01-12 09:49:55 -0600

    more progress on processing MQTT messages

--- mqtt-demo/index.js
+++ mqtt-demo/index.js
@@ -5,78 +5,8 @@
 // 2) optionally enter "java -jar TheJoveExpress.jar"
 //    to get lots of messages
 
-const TRAIN_NAME = 'thejoveexpress';
-
-//const mqttHost = TRAIN_NAME + '.local';
-const mqttHost = 'localhost';
-
-const mqtt = require('mqtt');
-const PORT = 1883;
-const client = mqtt.connect('mqtt://' + mqttHost + ':' + PORT);
-
-const topicPrefix = TRAIN_NAME + '/';
-
-function getTopic(name) {
-  return topicPrefix + (name ? name + '/' : '') + 'feedback';
-}
-
-const feedbackTopic = getTopic('');
-
-// To get a zero, kill train app.
-// To get a one, restart train app.
-const lifecycleTopic = getTopic('lifecycle');
-
-const lightsOverrideTopic = getTopic('lights/override');
-const lightsPowerTopic = getTopic('lights/power');
 /*
-const enginePowerTopic = getTopic('engine/power');
-const engineCalibrationTopic = getTopic('engine/calibration');
-const lightsAmbientTopic = getTopic('lights/ambient');
-const lightsCalibrationTopic = getTopic('lights/calibration');
-*/
-
-client.on('connect', () => {
-  const topic = TRAIN_NAME + '/#';
-  client.subscribe(topic);
-  //client.publish(topic, 'Hello from MQTT!');
-  client.publish('thejoveexpress/feedback');
-});
-
-// message is a Buffer
-client.on('message', (topic, message) => {
-  console.log('got message for topic', topic);
-  console.log('message length =', message.length);
-
-  switch (topic) {
-    case feedbackTopic:
-      break;
-
-    case lifecycleTopic:
-    case lightsPowerTopic: {
-      const value = message.readInt8(0);
-      console.log('lifecycle =', value);
-      break;
-    }
-
-    case lightsOverrideTopic: {
-      const value = message.readInt32BE(0);
-      console.log('lights override =', value);
-      break;
-    }
-
-    default: {
-      const num = message.readIntBE(0, 8);
-      console.log('num =', num);
-      const den = message.readIntBE(8, 8);
-      console.log('den =', den);
-      break;
-    }
-  }
-
-  //client.end();
-});
-
-/*
+Expected Messages:
 thejoveexpress/lifecycle/feedback - boolean, 1 byte
 thejoveexpress/engine/power/feedback - rational
 thejoveexpress/engine/calibration/feedback - rational
@@ -85,3 +15,145 @@ thejoveexpress/lights/override/feedback - enum, 4 bytes 0=off, 1=on, 2=auto
 thejoveexpress/lights/power/feedback - boolean, 1 byte
 thejoveexpress/lights/calibration/feedback - rational
 */
+
+const mqtt = require('mqtt');
+const MySqlConnection = require('mysql-easier');
+
+const MSG_DELIM = '/';
+const TRAIN_NAME = 'thejoveexpress';
+//const MQTT_HOST = TRAIN_NAME + '.local';
+const MQTT_HOST = 'localhost';
+const MQTT_PORT = 1883;
+
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'smartdevice'
+};
+const mySql = new MySqlConnection(dbConfig);
+
+const client = mqtt.connect('mqtt://' + MQTT_HOST + ':' + MQTT_PORT);
+
+const childMap = {};
+const parentMap = {};
+
+//const feedbackTopic = getTopic('');
+
+// To get a zero, kill train app.
+// To get a one, restart train app.
+const lifecycleTopic = getTopic('lifecycle');
+console.log('index.js x: lifecycleTopic =', lifecycleTopic);
+
+const lightsOverrideTopic = getTopic('lights', 'override');
+const lightsPowerTopic = getTopic('lights', 'power');
+const enginePowerTopic = getTopic('engine', 'power');
+const engineCalibrationTopic = getTopic('engine', 'calibration');
+const lightsAmbientTopic = getTopic('lights', 'ambient');
+const lightsCalibrationTopic = getTopic('lights', 'calibration');
+
+async function getChildId(parentName, childName) {
+  const key = parentName + MSG_DELIM + childName;
+  let id = childMap[key];
+  if (id) return id;
+
+  const parentId = await getParentId(parentName);
+  if (parentId === 0) return 0; // not found
+  const sql = 'select id from instance where parentId=? and name=?';
+  const rows = await mySql.query(sql, parentId, childName);
+  if (rows.length === 0) return 0; // not found
+  [{id}] = rows;
+  childMap[key] = id;
+  return id;
+}
+
+async function getParentId(parentName) {
+  let id = parentMap[parentName];
+  if (id) return id;
+
+  const sql = 'select id from instance where name=?';
+  const rows = await mySql.query(sql, parentName);
+  if (rows.length === 0) return 0; // not found
+  [{id}] = rows;
+  parentMap[parentName] = id;
+  return id;
+}
+
+function getTopic(...parts) {
+  const middle = parts.length ? parts.join(MSG_DELIM) + MSG_DELIM : '';
+  return TRAIN_NAME + MSG_DELIM + middle + 'feedback';
+}
+
+async function saveProperty(parentName, childName, property, value) {
+  const instanceId = childName
+    ? await getChildId(parentName, childName)
+    : await getParentId(parentName);
+  if (instanceId === 0) {
+    throw new Error(
+      'no instance found for ' + parentName + MSG_DELIM + childName
+    );
+  }
+
+  // Get the id of the existing instance_data row if any.
+  const sql = 'select id from instance_data where instanceId=?';
+  const rows = await mySql.query(sql, instanceId);
+  const id = rows.length ? rows[0].id : undefined;
+
+  const obj = {
+    id,
+    instanceId,
+    dataKey: property,
+    dataValue: value
+  };
+  mySql.upsert('instance_data', obj);
+}
+
+client.on('connect', () => {
+  //const topic = TRAIN_NAME + MSG_DELIM + '#';
+  const topic = '#';
+  client.subscribe(topic);
+  //client.publish(topic, 'Hello from MQTT!');
+  //client.publish(TRAIN_NAME + MSG_DELIM + 'feedback');
+});
+
+// message is a Buffer
+client.on('message', (topic, message) => {
+  //console.log('message length =', message.length);
+  const parts = topic.split('/');
+  parts.pop(); // removes "feedback" or "control" from end
+  const [parentName] = parts;
+  const hasChild = parts.length === 3;
+  const childName = hasChild ? parts[1] : undefined;
+  const property = hasChild ? parts[2] : parts[1];
+
+  let value;
+  switch (topic) {
+    case lifecycleTopic:
+    case lightsPowerTopic:
+      value = message.readInt8(0);
+      break;
+
+    case lightsOverrideTopic:
+      value = message.readInt32BE(0);
+      break;
+
+    case enginePowerTopic:
+    case engineCalibrationTopic:
+    case lightsAmbientTopic:
+    case lightsCalibrationTopic:
+      value = message.readIntBE(0, 8);
+      //const maxValue = message.readIntBE(8, 8);
+      //console.log('maxValue =', maxValue);
+      break;
+  }
+
+  if (value !== undefined) {
+    console.log(topic, '=', value);
+    saveProperty(parentName, childName, property, value);
+  } else {
+    console.error('unsupported topic', topic);
+  }
+
+  //client.end(); // only process first message
+});
+
