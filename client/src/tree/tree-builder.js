384f1f9190669c515867a685e1293391581db051
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-01-26 10:29:56 -0600

    changed to default to trees being fully expanded

--- client/src/tree/tree-builder.js
+++ client/src/tree/tree-builder.js
@@ -69,6 +69,8 @@ class TreeBuilder extends Component<PropsType> {
     dispatch('setNodes', payload);
 
     global.reloading = false;
+
+    this.toggleExpandAll();
   };
 
   toggleExpandAll = () => {
