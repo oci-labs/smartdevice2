566e2b67a9c21c50583e673d46789ab4c14d3adb
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-01-31 12:33:34 -0600

    extracted Header component from App component

--- client/src/app.js
+++ client/src/app.js
@@ -3,12 +3,11 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-import ImportExport from './import-export/import-export';
+import Header from './header/header';
 import InstanceDetail from './instance-detail/instance-detail';
 import InstanceHierarchy from './instance-hierarchy/instance-hierarchy';
 import LeftNav from './left-nav/left-nav';
-import Button from './share/button';
-import SdModal, {showModal} from './share/sd-modal';
+import SdModal from './share/sd-modal';
 import TypeAlerts from './type-alerts/type-alerts';
 import TypeProperties from './type-properties/type-properties';
 
@@ -17,7 +16,6 @@ import type {StateType, TreeType} from './types';
 import './app.css';
 
 type PropsType = {
-  mqttConnected: boolean,
   treeType: TreeType
 };
 
@@ -34,33 +32,11 @@ const rightMap = {
 };
 
 class App extends Component<PropsType> {
-  importExport = () => {
-    const renderFn = () => <ImportExport />;
-    showModal({title: 'Import/Export JSON Schema', renderFn});
-  };
-
   render() {
-    const {mqttConnected, treeType} = this.props;
-    const heartIcon = mqttConnected ? 'heartbeat' : 'heart-o';
-    const messageBrokerStatus = mqttConnected ? 'connected' : 'not running';
+    const {treeType} = this.props;
     return (
       <div className="app">
-        <header>
-          <img className="logo" alt="OCI logo" src="images/oci-logo.svg" />
-          <div className="title">Devo</div>
-          <div className="right">
-            <span
-              className={`fa fa-2x fa-${heartIcon}`}
-              title={`message broker is ${messageBrokerStatus}`}
-            />
-            <Button
-              className="import-export-btn fa-2x"
-              icon="cog"
-              onClick={() => this.importExport()}
-              tooltip="import/export"
-            />
-          </div>
-        </header>
+        <Header />
         <section id="body">
           <LeftNav />
           {middleMap[treeType]}
@@ -73,9 +49,8 @@ class App extends Component<PropsType> {
 }
 
 const mapState = (state: StateType): PropsType => {
-  const {mqttConnected, ui} = state;
-  const {treeType} = ui;
-  return {mqttConnected, treeType};
+  const {treeType} = state.ui;
+  return {treeType};
 };
 
 export default connect(mapState)(App);
---
+++ client/src/header/header.js
@@ -0,0 +1,64 @@
+// @flow
+
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import Button from '../share/button';
+import ImportExport from '../import-export/import-export';
+import {showModal} from '../share/sd-modal';
+
+import type {StateType} from '../types';
+
+import './header.css';
+
+type PropsType = {
+  mqttConnected: boolean
+};
+
+class Header extends Component<PropsType> {
+  importExport = () => {
+    const renderFn = () => <ImportExport />;
+    showModal({title: 'Import/Export JSON Schema', renderFn});
+  };
+
+  refresh = () => {
+    alert('Refresh is not implemented yet.');
+  };
+
+  render() {
+    const {mqttConnected} = this.props;
+    const heartIcon = mqttConnected ? 'heartbeat' : 'heart-o';
+    const messageBrokerStatus = mqttConnected ? 'connected' : 'not running';
+    return (
+      <header className="header">
+        <img className="logo" alt="OCI logo" src="images/oci-logo.svg" />
+        <div className="title">Devo</div>
+        <div className="right">
+          <span
+            className={`fa fa-2x fa-${heartIcon}`}
+            title={`message broker is ${messageBrokerStatus}`}
+          />
+          <Button
+            className="refresh-btn fa-2x"
+            icon="refresh"
+            onClick={() => this.refresh()}
+            tooltip="import/export"
+          />
+          <Button
+            className="import-export-btn fa-2x"
+            icon="cog"
+            onClick={() => this.importExport()}
+            tooltip="import/export"
+          />
+        </div>
+      </header>
+    );
+  }
+}
+
+const mapState = (state: StateType): PropsType => {
+  const {mqttConnected} = state;
+  return {mqttConnected};
+};
+
+export default connect(mapState)(Header);
